87da8c708d190e09585efe693dffef1391d342c6
SACHEEN PATIL <mrsacheenpatil@gmail.com>
2026-01-11 12:55:56 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   test: {
     environment: "jsdom",
     globals: true,
@@ -13,3 +14,4 @@ export default defineConfig({
     }
   }
 });
+
